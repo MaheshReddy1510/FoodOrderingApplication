af686f76af618ecdf3ab3b5c1a8795d09814db67
Mahesh Reddy Lattupally <maheshreddylatupally@gmail.com>
2017-10-04 17:19:02 -0400

    Route to Cusisine was Added

--- FoodOrderingApplication/App_Start/RouteConfig.cs
+++ FoodOrderingApplication/App_Start/RouteConfig.cs
@@ -13,6 +13,12 @@ namespace FoodOrderingApplication
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapRoute("Cusisine",
+                "cuisine/{name}",
+                new {controller="Cuisine",action="Search",name=UrlParameter.Optional
+                }
+                );
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
